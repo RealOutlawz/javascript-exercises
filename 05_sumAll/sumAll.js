f53fe143e453b65bb5d2b6b807f0fdfa2ed0fb21
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-11 22:53:23 -0500

    Condensed checks into one function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,19 @@
 const sumAll = function (num1, num2) {
-  if (typeof num1 !== "number" || typeof num2 !== "number") {
-    return "ERROR";
-  } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+  function isNumberInteger(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+      return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+      return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+      return "ERROR";
+    } else {
+      return true;
+    }
+  }
+  const checks = isNumberInteger(num1, num2);
+  if (checks === "ERROR") {
     return "ERROR";
   }
-
-  if (num1 < 0 || num2 < 0) {
-    return "ERROR";
-  }
-
   let sum = 0;
   let bigger;
   let smaller;
