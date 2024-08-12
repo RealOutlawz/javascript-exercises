00e733a85db02b0d4ed063d8e032e823a096b10b
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-11 22:34:55 -0500

    Paased 4 of the 7 tests so far

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+    return "ERROR";
+  }
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  let bigger;
+  let smaller;
+
+  if (num1 > num2) {
+    bigger = num1;
+    smaller = num2;
+  } else {
+    bigger = num2;
+    smaller = num1;
+  }
+  for (let i = smaller; i <= bigger; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
+sumAll(1, 4);
+//returns ERROR with negative numbers
+//(-10, 4) yes
+// returns ERROR with non-integer parameters
+//(2.5, 4) no
+// returns ERROR with non-number parameters
+//(10, "90")no
+// returns ERROR with non-number parameters
+//(10, [90, 1]) haven't tested this yet
 // Do not edit below this line
 module.exports = sumAll;
