1ed1ebf42b2599008a673b04a4a457c04296af00
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-12 18:06:04 -0500

    Passed all test on exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (num, num2) {
+  return num + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num, num2) {
+  return num - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num, num2) {
+  let power = 1;
+  for (let i = 0; i < num2; i++) {
+    power *= num;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let number = 1;
+  if (num == 0) {
+    return number;
+  }
+  while (num > 0) {
+    number *= num;
+    num--;
+  }
+  return number;
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
