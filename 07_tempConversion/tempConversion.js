ed393277db835cfb84a012506aa7a6147c7538c9
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-12 17:27:13 -0500

    Passed all test for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  const conversion = ((num - 32) * 5) / 9;
+  return parseFloat(conversion.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  const conversion = (num * 9) / 5 + 32;
+  return parseFloat(conversion.toFixed(1));
 };
 
+convertToCelsius(32); // fahrenheit to celsius, should return 0
+convertToFahrenheit(0); // celsius to fahrenheit, should return 32
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
