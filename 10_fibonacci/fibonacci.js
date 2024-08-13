73e6098bb65019dca6d7365d0a23016189c1edeb
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-12 22:44:03 -0500

    Passed all test are exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,15 +1,18 @@
 const fibonacci = function (num) {
-  let counter = 0;
   if (num == 0) {
     return (num = 0);
   } else if (num < 0) {
     return "OOPS";
   } else {
-    for (let i = 1; i <= num; i++) {
-      let prevNum;
-      let currentNum;
-      let sum;
+    let prevNum = 0;
+    let currentNum = 1;
+    let sum = 1;
+    for (let i = 1; i < num; i++) {
+      sum = prevNum + currentNum;
+      prevNum = currentNum;
+      currentNum = sum;
     }
+    return sum;
   }
 };
 fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
@@ -27,7 +30,7 @@ doesn\'t accept negatives
 - "OOPS"
 DOES accept strings
 - "0" 0
-- "1" 1
+- "1" 1 x
 - "2" 1
 - "8" 21
 */
@@ -38,8 +41,25 @@ create if statement to check if number is 0
 create if statement to check if number is negative
 create a for loop that iterates from 1 to the number
 create the variables prevNum, currentNum, and sum
-
-
-
-
+current num = 1
+prev num = 0
+in the loop 
+sum = prevNum + currentNum
+prevnum = currentNum
+current num = sum
+current num = 1
+prev num = 1
+sum = 2
+prevnum = 1
+current num = 2
+sum = 3
+prevnum = 2
+curenet num = 3
+sum = 5
+prevnum = 3
+current num = 5
+sum = 8
+prev num = 5
+curennt num = 8
+sum = 13
 */
